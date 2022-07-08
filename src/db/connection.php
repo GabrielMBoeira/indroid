281b7cd93b90ecba1e9a967d4c8dc7c7fc0d0a58
Gabriel <gabriel.boeira@softplan.com.br>
2022-07-07 23:43:01 -0300

    Configurando banco heroku

--- src/db/connection.php
+++ src/db/connection.php
@@ -1,28 +1,9 @@
 <?php
 
-function newConnection($banco = 'indroid') {
-    $servidor = 'localhost';
-    $usuario = 'root';
-    $senha = '';
-
-    $conn = new mysqli($servidor, $usuario, $senha, $banco);
-
-    if($conn->connect_error) {
-        die('Erro: ' . $conn->connect_error);
-    }
-
-    return $conn;
-}
-
-
-// -------------------------------------------------------------------------
-// PRODUÇÃO
-// -------------------------------------------------------------------------
-
 // function newConnection($banco = 'indroid') {
-//     $servidor = 'indroid.cj69zfae2few.us-east-1.rds.amazonaws.com';
-//     $usuario = 'indroid';
-//     $senha = 'CHAngeme1*';
+//     $servidor = 'localhost';
+//     $usuario = 'root';
+//     $senha = '';
 
 //     $conn = new mysqli($servidor, $usuario, $senha, $banco);
 
@@ -32,3 +13,22 @@ function newConnection($banco = 'indroid') {
 
 //     return $conn;
 // }
+
+
+// -------------------------------------------------------------------------
+// PRODUÇÃO
+// -------------------------------------------------------------------------
+
+function newConnection($banco = 'heroku_8158a15a8674c96') {
+    $servidor = 'us-cdbr-east-06.cleardb.net';
+    $usuario = 'be0146cccd6e81';
+    $senha = '98cc115c';
+
+    $conn = new mysqli($servidor, $usuario, $senha, $banco);
+
+    if($conn->connect_error) {
+        die('Erro: ' . $conn->connect_error);
+    }
+
+    return $conn;
+}
