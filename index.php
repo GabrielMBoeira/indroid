a8084ca9818c0936c4cff168256da9428a6e9b01
Gabriel <gabriel.boeira@softplan.com.br>
2022-07-08 01:26:04 -0300

    teste heroku

--- index.php
+++ index.php
@@ -3,11 +3,10 @@
 $url = (isset($_GET['url']) ? $_GET['url'] : 'home');
 $url = array_filter(explode('/', $url)); 
 
-var_dump($url);
-die;
-
 $file_url = $url[0].'.php';
 $file_path = 'src/view/'.$file_url;
+
+var_dump($file_path);
 
 if (is_file($file_path)) {
     require_once($file_path);
