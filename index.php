5ee513d8dd2d35730b2f1ecf448a9e338ce396bb
Gabriel Machado Boeira <gabriel.boeira@softplan.com.br>
2023-04-20 20:43:36 -0300

    add index redirect htaccess

--- index.php
+++ index.php
@@ -1,17 +1,14 @@
 <?php
 
-echo 'aqui';
+$url = (isset($_GET['url']) ? $_GET['url'] : 'home');
+$url = array_filter(explode('/', $url)); 
 
+$file_url = $url[0].'.php';
+$file_path = 'src/view/'.$file_url;
 
-// $url = (isset($_GET['url']) ? $_GET['url'] : 'home');
-// $url = array_filter(explode('/', $url)); 
-
-// $file_url = $url[0].'.php';
-// $file_path = 'src/view/'.$file_url;
-
-// if (is_file($file_path)) {
-//     require_once($file_path);
-// } else {
-//     require_once('src/view/404.php');
-// }
+if (is_file($file_path)) {
+    require_once($file_path);
+} else {
+    require_once('src/view/404.php');
+}
 
