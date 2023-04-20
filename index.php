4dbc5772c0d22f64b1e594fe03685a84606cf523
Gabriel Machado Boeira <gabriel.boeira@softplan.com.br>
2023-04-20 20:32:23 -0300

    teste

--- index.php
+++ index.php
@@ -1,14 +1,17 @@
 <?php
 
-$url = (isset($_GET['url']) ? $_GET['url'] : 'home');
-$url = array_filter(explode('/', $url)); 
+echo 'aqui';
 
-$file_url = $url[0].'.php';
-$file_path = 'src/view/'.$file_url;
 
-if (is_file($file_path)) {
-    require_once($file_path);
-} else {
-    require_once('src/view/404.php');
-}
+// $url = (isset($_GET['url']) ? $_GET['url'] : 'home');
+// $url = array_filter(explode('/', $url)); 
+
+// $file_url = $url[0].'.php';
+// $file_path = 'src/view/'.$file_url;
+
+// if (is_file($file_path)) {
+//     require_once($file_path);
+// } else {
+//     require_once('src/view/404.php');
+// }
 
