0c8a3d5d1474bfe7e70189372a1b1cef24667793
GabrielMBoeira <gboeira777@gmail.com>
2023-08-20 13:23:39 -0300

    Send email configuration

--- src/mail/send.php
+++ src/mail/send.php
@@ -4,124 +4,67 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
-// require_once(__DIR__ . '/vendor/autoload.php');
+function sendForgotEmail($email, $hash)
+{
 
-// // Configure API key authorization: api-key
-// $config = SendinBlue\Client\Configuration::getDefaultConfiguration()->setApiKey('api-key', 'YOUR_API_KEY');
-// // Uncomment below to setup prefix (e.g. Bearer) for API key, if needed
-// // $config = SendinBlue\Client\Configuration::getDefaultConfiguration()->setApiKeyPrefix('api-key', 'Bearer');
-// // Configure API key authorization: partner-key
-// $config = SendinBlue\Client\Configuration::getDefaultConfiguration()->setApiKey('partner-key', 'YOUR_API_KEY');
-// // Uncomment below to setup prefix (e.g. Bearer) for API key, if needed
-// // $config = SendinBlue\Client\Configuration::getDefaultConfiguration()->setApiKeyPrefix('partner-key', 'Bearer');
+    $envPath = realpath(dirname(__FILE__, 3) . '/.env');
+    $env = parse_ini_file($envPath);
 
-// $apiInstance = new SendinBlue\Client\Api\AccountApi(
-//     // If you want use custom http client, pass your client which implements `GuzzleHttp\ClientInterface`.
-//     // This is optional, `GuzzleHttp\Client` will be used as default.
-//     new GuzzleHttp\Client(),
-//     $config
-// );
+    $username = $env['USERNAME'];
+    $password = $env['PASSWORD'];
 
-// try {
-//     $result = $apiInstance->getAccount();
-//     print_r($result);
-// } catch (Exception $e) {
-//     echo 'Exception when calling AccountApi->getAccount: ', $e->getMessage(), PHP_EOL;
-// }
+    //Load Composer's autoloader
+    require_once(dirname(__FILE__, 3) . '/vendor/autoload.php');
 
+    $mail = new PHPMailer(true);
 
+    try {
 
+        $mail->SMTPDebug = SMTP::DEBUG_SERVER;      //Enable verbose debug output
+        $mail->isSMTP();                            //Send using SMTP
+        $mail->Host       = 'smtp.hostinger.com';   //Set the SMTP server to send through
+        $mail->SMTPAuth   = true;                   //Enable SMTP authentication
+        $mail->Username   = $username;              //SMTP username
+        $mail->Password   = $password;              //SMTP password
+        $mail->SMTPSecure =  'ssl';                 //Enable ssl encryption
+        $mail->Port       = 465;                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+        $mail->CharSet = 'UTF-8';
 
+        $name_from = 'Indroid';
+        $email_from = 'suporte@indroid.com.br';
+        $email_to = $email;
 
-// // include 'path/to/mailin-api/Mailin.php';
-// $mailin = new Mailin('indroidbot@gmail.com', 'bFCLRqthXxvzyOUP');
-// $mailin->
-// addTo('indroidbot@gmail.com', 'Indroid Bot')->
-// setFrom('indroidbot@gmail.com', 'Indroid Bot')->
-// setReplyTo('indroidbot@gmail.com','Indroid Bot')->
-// setSubject('Inserir o assunto aqui')->
-// setText('Olá')->
-// setHtml('<strong>Olá</strong>');
-// $res = $mailin->send();
+        //Recipients
+        $mail->setFrom($email_from, $name_from);
+        $mail->addAddress($email_to);
+        // $mail->addReplyTo('ellen@example.com', 'Indroid');
+        // $mail->addCC('cc@example.com');
+        // $mail->addBCC('bcc@example.com');
 
-// As mensagens de sucesso foram reenviadas sob esta forma:
-// {'result' => true, 'message' => 'E-mail enviado'}
+        //Attachments
+        // $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+        // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
 
+        $mail->isHTML(true);
+        // $mail->WordWrap = 50;  
 
+        $mail->Subject = 'introdução';
+        $mail->Body    = "
+                            Olá! 
+                            <br>
+                            <br>
+                            <b>Vocêee solicitou a recuperação de senha para o aplicativo inDroid!</b>
+                            <p>Observação: Para efetuar a recuperação de senha clique no link abaixo.</p>
+                            <br>
+                            <br>
+                            <a href='https://www.indroid.com.br/alter_password_forgot?user=" . $email . "&key=" . $hash . "'>Clique aqui para recuperar senha!</a>
+                            ";
 
+        // $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
+        $mail->send();
 
-
-
-function sendForgotEmail($email, $hash) {
-
-$envPath = realpath(dirname(__FILE__, 3) . '/.env');
-$env = parse_ini_file($envPath);
-
-$username = $env['USERNAME'];
-$password = $env['PASSWORD'];
-
-//Load Composer's autoloader
-require_once(dirname(__FILE__, 3) . '/vendor/autoload.php');
-
-//Create an instance; passing `true` enables exceptions
-$mail = new PHPMailer(true);
-
-try {
-    //Server settings
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                //Enable verbose debug output
-    $mail->isSMTP();                                      //Send using SMTP
-    $mail->Host       = 'smtp.hostinger.com';             //Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                             //Enable SMTP authentication
-    $mail->Username   = $username;                        //SMTP username
-    $mail->Password   = $password;                        //SMTP password
-    $mail->SMTPSecure =  PHPMailer::ENCRYPTION_STARTTLS;  //Enable implicit TLS encryption
-    $mail->Port       = 465;                              //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
-
-    $name_from = 'Indroid';
-    $email_from = 'suporte@indroid.com.br';
-    // $email_to = $email;
-    $email_to = 'gabrielmboeira@gmail.com';
-
-    //Recipients
-    $mail->setFrom($email_from , $name_from);
-    $mail->addAddress($email_to);    
-    // $mail->addAddress('ellen@example.com');                  
-    // $mail->addReplyTo('ellen@example.com', 'Indroid');
-    // $mail->addCC('cc@example.com');
-    // $mail->addBCC('bcc@example.com');
-
-    //Attachments
-    // $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
-    // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
-
-    //Content
-    $mail->isHTML(true);   //Set email format to HTML
-    // $mail->WordWrap = 50;  
-
-    $mail->Subject = 'Recuperação de senha - Indroid';
-    $mail->Body    = "
-    
-    Olá! 
-    <br>
-    <br>
-    <b>Você solicitou a recuperação de senha para o aplicativo inDroid!</b>
-    <p>Observação: Para efetuar a recuperação de senha clique no link abaixo.</p>
-    <br>
-    <br>
-    <a href='https://www.indroid.com.br/alter_password_forgot?user=".$email."&key=".$hash."'>Clique aqui para recuperar senha!</a>
-
-
-    ";
-    // $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
-    // var_dump("aqui");
-    // die;
-
-    $mail->send();
-    echo utf8_encode("Mensagem enviada com sucesso para $email_to!");
-} catch (Exception $e) {
-    echo utf8_encode("Mensagem não pode ser enviada para $email_to!. Mailer Error: {$mail->ErrorInfo}");
+    } catch (Exception $e) {
+        echo "Mensagem não pode ser enviada para $email_to!. Mailer Error: {$mail->ErrorInfo}";
+    }
 }
-
-}  
